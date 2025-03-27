cc64e70620234c602009d73e1131799033174648
bxbach732 <buixuanbach2000@gmail.com>
2025-03-27 03:33:24 +0200

    feat: update all the tasks' links

--- landing_page/src/components/CardList.jsx
+++ landing_page/src/components/CardList.jsx
@@ -9,7 +9,7 @@ const CardList = () => {
       link: "./tg.png",
       image_credit:
         "Image credit: Image by anonymous; CC BY-ND 4.0; https://emojis.sh/emoji/tangram-8F7UHXt",
-      task_link: "https://tangram-arena-m41-tangram-arena.node01.ki-lab.nrw",
+      task_link: "https://tangram-mar1-tangram-pl-mar.node01.ki-lab.nrw/",
       code_link:
         "https://github.com/AaltoRSE/CollaborativeAI/tree/main/task_template/frontend_tangram",
     },
@@ -19,7 +19,7 @@ const CardList = () => {
       link: "./pt.png",
       image_credit:
         "Image credit: Image by e_s ekaterina_s; CC BY-ND 4.0; https://emoiis.sh/emoji/a-feather-quill-and-an-open-scroll-IZrNcJ6eFQ",
-      task_link: "https://poetry-task-arena1-poetry-arena-m4.node01.ki-lab.nrw",
+      task_link: "https://poetry-mar1-poetry-pl-mar.node01.ki-lab.nrw/",
       code_link:
         "https://github.com/AaltoRSE/CollaborativeAI/tree/main/task_template/frontend_poetry",
     },
@@ -29,7 +29,7 @@ const CardList = () => {
       link: "./mp.png",
       image_credit:
         "Image credit: Image by carladanielewic; CC BY-ND 4.0; https://www.emojis.com/emoji/meal-plan-paper-d99RHsRlOSD",
-      task_link: "https://mealplan-task1-mealplan-arena.node01.ki-lab.nrw",
+      task_link: "https://mealplan-mar1-mealplan-pl-mar.node01.ki-lab.nrw/",
       code_link:
         "https://github.com/AaltoRSE/CollaborativeAI/tree/main/task_template/frontend_mealplan",
     },
@@ -39,7 +39,7 @@ const CardList = () => {
       link: "./rp.png",
       image_credit:
         "Image credit: Image by richardbeaurega; CC BY-ND 4.0; https://www.emojis.com/emoji/recipe-book-GsQnkLyZfJ",
-      task_link: "#",
+      task_link: "https://recipe-mar1-recipe-pl-mar.node01.ki-lab.nrw/",
       code_link:
         "https://github.com/AaltoRSE/CollaborativeAI/tree/main/task_template/frontend_recipe",
     }
